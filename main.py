f898247455dca41e68914d201826ee6e8e527fe9
Denif Faller <richsiteru@gmail.com>
2022-08-26 15:30:30 +0700

    refactoring

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
+from translate import Translate
 import json
 import logging
 import database_lib
+import sys
 
 app = Flask(__name__)
 
@@ -22,17 +24,13 @@ def main():
     return json.dumps(response)
 
 connection = 0
-cntWords = 0
-words = []
-idCurWord = 0
-curWord = ""
-nextWord = ""
-translationOptions = ""
 LIMIT_WORDS = 11
 sessionLimit = LIMIT_WORDS
+trasnslateObject = Translate()
 
 def getWords():
-    global connection, words, LIMIT_WORDS
+    global connection, LIMIT_WORDS, trasnslateObject
+
     f = open('config.txt')
     config = []
     for line in f:
@@ -40,118 +38,120 @@ def getWords():
     connection = database_lib.createConnection(config[0], config[1], config[2].strip(), config[3])
     useDatabaseQuery = "USE skill_alice"
     database_lib.useDatabase(connection, useDatabaseQuery)
-    selectQuery = "SELECT * FROM words WHERE status = 2 LIMIT " + str(LIMIT_WORDS - 6)
-    words = database_lib.selectRows(connection, selectQuery)
-    lenInCorrectWords = len(words)
+    selectQuery = "SELECT * FROM words WHERE status = 2 LIMIT " + str(LIMIT_WORDS - 1)
+    trasnslateObject.words = database_lib.selectRows(connection, selectQuery)
+    lenInCorrectWords = len(trasnslateObject.words)
     selectQuery = "SELECT * FROM words WHERE status = 0 LIMIT " + str(LIMIT_WORDS - lenInCorrectWords - 1)
-    words.extend(database_lib.selectRows(connection, selectQuery))
+    trasnslateObject.words.extend(database_lib.selectRows(connection, selectQuery))
     #logging.error('%s raised an error',  words)
 
 def getWord():
-    global cntWords, words, idCurWord, curWord, nextWord, translationOptions
+    global trasnslateObject
+
     #logging.error('%s words',  words)
-    idCurWord = words[0][0]
-    curWord = words[0][1] 
-    if len(words) > 1:
-        nextWord = words[1][1] 
+    trasnslateObject.idCurWord = trasnslateObject.words[0][0]
+    trasnslateObject.curWord = trasnslateObject.words[0][1]
+    if len(trasnslateObject.words) > 1:
+        trasnslateObject.nextWord = trasnslateObject.words[1][1]
     else:
-        nextWord = words[0][1] 
-    translationOptions = words[0][2]
-    translationOptions = translationOptions.replace("[", "").replace("]", "").replace('"', "").replace("'", "").replace(" ", "").replace("'", "").split(",")
-    words.pop(0)
-    return [idCurWord, nextWord, translationOptions]
-    
+        trasnslateObject.nextWord = trasnslateObject.words[0][1]
+    trasnslateObject.translationOptions = trasnslateObject.words[0][2]
+    trasnslateObject.translationOptions = trasnslateObject.translationOptions.replace("[", "").replace("]", "").replace('"', "").replace("'", "").replace(" ", "").replace("'", "").split(",")
+    trasnslateObject.words.pop(0)
+    return [trasnslateObject.idCurWord, trasnslateObject.nextWord, trasnslateObject.translationOptions]
+
 def resultAnswer(res, requestText):
-    global connection, words, idCurWord, curWord, nextWord, translationOptions, sessionLimit
-    
+    global connection, sessionLimit, trasnslateObject
+
     isHasInTranslationOptions = False
-    for wordItem in translationOptions:
+    for wordItem in trasnslateObject.translationOptions:
         #logging.error('%s raised an error',  wordItem)
         #logging.error('%s raised an error',  wordItem == requestText)
         if wordItem == requestText:
             isHasInTranslationOptions = True
-            
-    correctAnswer = translationOptions[0]        
-    
+
+    correctAnswer = trasnslateObject.translationOptions[0]
+
     #logging.error('%s translationOptions', translationOptions)
     #logging.error('%s cntWords',  cntWords)
     #logging.error('%s sessionLimit',  sessionLimit)
     #logging.error('%s len',  len(words))
     if (isHasInTranslationOptions) and (requestText != ""):
-        if cntWords == (sessionLimit - 1):
+        if trasnslateObject.cntWords == (sessionLimit - 1):
             res['response']['text'] = "Правильно. Будем продолжать?\n"
-        elif len(words) == 0:
-            res['response']['text'] = "Правильно. Слов для изучения нет\n"    
+        elif len(trasnslateObject.words) == 0:
+            res['response']['text'] = "Правильно. Слов для изучения нет\n"
         else:
-            res['response']['text'] = "Правильно.\nПереведи слово - " + nextWord
-        updateRowQuery = f"UPDATE words SET status = 1 WHERE id = " + str(idCurWord)
+            res['response']['text'] = "Правильно.\nПереведи слово - " + trasnslateObject.nextWord
+        updateRowQuery = f"UPDATE words SET status = 1 WHERE id = " + str(trasnslateObject.idCurWord)
         database_lib.updateRow(connection, updateRowQuery, "words")
     else:
-        if cntWords == (sessionLimit - 1):
+        if trasnslateObject.cntWords == (sessionLimit - 1):
             res['response']['text'] = "Неправильно. Правильно {0}. Будем продолжать?\n".format(correctAnswer)
-        elif len(words) == 0:
+        elif len(trasnslateObject.words) == 0:
             res['response']['text'] = "Неправильно. Правильно {0}. Слов для изучения нет\n".format(correctAnswer)
         else:
-            res['response']['text'] = "Неправильно. Правильно {0}. Переведи слово - {1}".format(correctAnswer, nextWord)
-        #logging.error('%s idCurWord',  idCurWord)    
-        updateRowQuery = f"UPDATE words SET status = 2 WHERE id = " + str(idCurWord)
+            res['response']['text'] = "Неправильно. Правильно {0}. Переведи слово - {1}".format(correctAnswer, trasnslateObject.nextWord)
+        #logging.error('%s idCurWord',  idCurWord)
+        updateRowQuery = f"UPDATE words SET status = 2 WHERE id = " + str(trasnslateObject.idCurWord)
         database_lib.updateRow(connection, updateRowQuery, "words")
-        words.append((idCurWord, curWord, str(translationOptions)))
+        trasnslateObject.words.append((trasnslateObject.idCurWord, trasnslateObject.curWord, str(trasnslateObject.translationOptions)))
         sessionLimit +=1
-    if cntWords != (sessionLimit - 1):
-        if len(words) != 0:
+    if trasnslateObject.cntWords != (sessionLimit - 1):
+        if len(trasnslateObject.words) != 0:
             listWord = getWord()
-            idCurWord = listWord[0]
-            nextWord = listWord[1]
-            translationOptions = listWord[2]
-    elif len(words) != 0:
-        words.pop(0)
+            trasnslateObject.idCurWord = listWord[0]
+            trasnslateObject.nextWord = listWord[1]
+            trasnslateObject.translationOptions = listWord[2]
+    elif len(trasnslateObject.words) != 0:
+        trasnslateObject.words.pop(0)
 
 # Добавить кнопку завершить, да-нет
 def handleDialog(res, req):
-    global cntWords, words, idCurWord, nextWord, translationOptions, LIMIT_WORDS, sessionLimit
+    global trasnslateObject, LIMIT_WORDS, sessionLimit
+
     requestText = req['request']['original_utterance'].lower()
-    
+
     #logging.error('%s words', words)
-    if cntWords > 0:
+    if trasnslateObject.cntWords > 0:
         if requestText == "завершить":
             res['response']['text'] = "Отлично. Если будет скучно, потренируйся"
         else:
             #logging.error('%s cntWords', cntWords)
-            if  cntWords == sessionLimit:
+            if  trasnslateObject.cntWords == sessionLimit:
                 if requestText == "нет":
                     res['response']['text'] = "Отлично потренировались. Не забывай повторять"
                 else:
                     getWords()
-                    if len(words) == 0:
+                    if len(trasnslateObject.words) == 0:
                         res['response']['text'] = "Слов для изучения нет"
                         return
                     listWord = getWord()
-                    idCurWord = listWord[0]
-                    nextWord = listWord[1]
-                    translationOptions = listWord[2]
-                    res['response']['text'] = "Продолжаем.\nПереведи слово - " + nextWord
-                    cntWords = 1
+                    trasnslateObject.idCurWord = listWord[0]
+                    trasnslateObject.nextWord = listWord[1]
+                    trasnslateObject.translationOptions = listWord[2]
+                    res['response']['text'] = "Продолжаем.\nПереведи слово - " + trasnslateObject.nextWord
+                    trasnslateObject.cntWords = 1
                     sessionLimit = LIMIT_WORDS
-            elif cntWords < sessionLimit:
+            elif trasnslateObject.cntWords < sessionLimit:
                 #logging.error('%s raised an error', words[wordIndex][1])
                 #logging.error('%s raised an error', words[wordIndex - 1])
                 #logging.error('%s raised an error', wordIndex)
                 resultAnswer(res, requestText)
-                cntWords+= 1
+                trasnslateObject.cntWords+= 1
     else:
         getWords()
         #logging.error('%s raised an error', words)
-        if len(words) == 0:
+        if len(trasnslateObject.words) == 0:
             res['response']['text'] = "Слов для изучения нет"
             return
-        nextWord = words[0][1]
-        res['response']['text'] = "Здравствуй. Давай начнем учить английский вместе со мной.\nПереведи слово - " + nextWord
+        trasnslateObject.nextWord = trasnslateObject.words[0][1]
+        res['response']['text'] = "Здравствуй. Давай начнем учить английский вместе со мной.\nПереведи слово - " + trasnslateObject.nextWord
         listWord = getWord()
-        idCurWord = listWord[0]
-        nextWord = listWord[1]
-        translationOptions = listWord[2]
-        cntWords+= 1
+        trasnslateObject.idCurWord = listWord[0]
+        trasnslateObject.nextWord = listWord[1]
+        trasnslateObject.translationOptions = listWord[2]
+        trasnslateObject.cntWords += 1
 
 if __name__ == '__main__':
     app.run()
---
+++ translate.py
@@ -0,0 +1,7 @@
+class Translate:
+    cntWords = 0
+    words = []
+    idCurWord = 0
+    curWord = ""
+    nextWord = ""
+    translationOptions = ""
